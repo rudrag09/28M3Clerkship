2aca84b01aec49b069c54dd72521707443cbdb25
rudrag09 <rudragandhi345425@gmail.com>
2026-03-06 17:59:55 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const config = {
-  supabaseUrl: 'YOUR_SUPABASE_URL',
-  supabaseAnonKey: 'YOUR_SUPABASE_ANON_KEY',
+  supabaseUrl: 'https://mthazydsgefgyjgedpob.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10aGF6eWRzZ2VmZ3lqZ2VkcG9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MzEwOTUsImV4cCI6MjA4ODQwNzA5NX0.YUajvabwPIVNAQ7XWK4b0RmWE1rfSLA-BSm6Lzt9c9Q',
   allowedEmailDomains: ['wright.edu'],
   appName: 'Clerkship Overlap',
   redirectTo: window.location.origin + window.location.pathname,
